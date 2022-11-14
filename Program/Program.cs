3d9e766a02aba5a897a244384b113131118adf95
VladimirE <wortex2014@bk.ru>
2022-11-15 01:19:50 +0400

    itog

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,51 +1,28 @@
-﻿Console.WriteLine("Введите длину массива");
-int num = Convert.ToInt32(Console.ReadLine());
-string[] handArray = new string[num];
-FillStringArray(handArray);
-Console.Write("[" + string.Join(", ", handArray) + "]");
-Console.WriteLine();
-int numLen = 3;
-int NewArrayLen = CountNewArrayLenght(handArray, numLen);
-Console.WriteLine(NewArrayLen);
-
-if (NewArrayLen == 0) Console.WriteLine("[]");
-else
-{
-    string[] newArray = new string[NewArrayLen];
-    FillNewStringArray(handArray, newArray, numLen);
-    Console.Write("[" + string.Join(", ", newArray) + "]");
-    Console.WriteLine();
-}
-
-void FillStringArray(string[] array)
+﻿void InputArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
-    {
-        Console.WriteLine($"{i + 1}. Введите значение массива");
-        array[i] = Console.ReadLine();
+    {   
+        Console.WriteLine("Введите " + i + " элемент массива: ");
+        array[i] = Convert.ToString(Console.ReadLine()!);
     }
 }
-
-
-int CountNewArrayLenght(string[] arr, int numLenght)
+void NewArray (string [] array)
 {
-    int counter = 0;
-    for (int i = 0; i < arr.Length; i++)
-    {
-        if (arr[i].Length <= numLenght) counter++;
-    }
-    return counter;
-}
-
-void FillNewStringArray(string[] arrayOld, string[] arrayNew, int numStringLen)
-{
-    int newArrayIndex = 0;
-    for (int i = 0; i < arrayOld.Length; i++)
-    {
-        if (arrayOld[i].Length <= numStringLen)
-        {
-            arrayNew[newArrayIndex] = arrayOld[i];
-            newArrayIndex++;
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+        { 
+            if (array[i].Length < 4)
+                Console.Write($"{array[i]} ");  
         }
-    }
+    Console.Write("]");
 }
+
+
+Console.WriteLine("Сколько элментов массива вы хотите ввести?");
+int M = Convert.ToInt32(Console.ReadLine());
+string [] array = new string[M];
+InputArray(array);
+Console.WriteLine();
+Console.WriteLine($"Введённый массив: [{string.Join(", ", array)}]");
+Console.Write("Полученный массив: ");
+NewArray(array);
